6dc784e33ff27e324c7cd1455c4f5e65ab91d1da
kory-job <catcher.intherye.rk@gmail.com>
2021-06-10 10:32:30 +0900

    ログイン後ページ遷移編集

--- front/front-app/src/components/auth/Auth.jsx
+++ front/front-app/src/components/auth/Auth.jsx
@@ -1,12 +1,13 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { loggedInStatus } from "../../reducks/users/operations";
+import { completedLoggedInStatus, loggedInStatus } from "../../reducks/users/operations";
 
 const Auth = ({children}) => {
   const dispatch =  useDispatch();
 
   useEffect(()=> {
     dispatch(loggedInStatus())
+    dispatch(completedLoggedInStatus())
   },[])
 
   return children
