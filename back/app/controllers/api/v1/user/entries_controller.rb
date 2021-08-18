31f8d802732a074b1938b4b04e62ec0aaacd52a3
kory-job <catcher.intherye.rk@gmail.com>
2021-08-18 11:33:17 +0900

    ローカル変数修正

--- back/app/controllers/api/v1/user/entries_controller.rb
+++ back/app/controllers/api/v1/user/entries_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::User::EntriesController < Api::V1::User::Base
 
   def check
     user = User.find(params[:id])
+    is_room = false
     current_user_entries = Entry.where(user_id: current_user.id)
     user_entries = Entry.where(user_id: user.id)
     unless user.id == current_user.id
