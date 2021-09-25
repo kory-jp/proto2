419629018fca0b1b5b742d7b8adca75b0f258c14
kory-job <catcher.intherye.rk@gmail.com>
2021-09-25 10:52:48 +0900

    SSL化

--- back/config/initializers/cors.rb
+++ back/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'http://with-accountant.com/'
+    origins 'http://localhost:3000', 'https://with-accountant.com/'
 
     resource '*',
       headers: :any,
