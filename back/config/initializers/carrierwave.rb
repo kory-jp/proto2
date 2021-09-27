a46578ae05bf65817550c2808f6c01a660553b94
kory-job <catcher.intherye.rk@gmail.com>
2021-09-27 11:07:09 +0900

    S3設定ファイル作成

--- back/config/initializers/carrierwave.rb
+++ back/config/initializers/carrierwave.rb
@@ -1,5 +1,26 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+
 CarrierWave.configure do |config|
-  config.asset_host = "http://localhost:3001"
-  config.storage = :file
-  config.cache_storage = :file
+  if Rails.env.production?
+    config.storage :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory  = 'portfolio-proto2-s3' # バケット名
+    config.fog_public = false
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['S3_ACCESS_KEY_ID'], # アクセスキー
+      aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'], # シークレットアクセスキー
+      region: 'ap-northeast-1', # リージョン
+      path_style: true
+    }
+
+  else
+    config.asset_host = "http://localhost:3001"
+    config.storage = :file
+    config.cache_storage = :file
+    config.ennable_processing = false if Rails.env.test?
+  end
 end
